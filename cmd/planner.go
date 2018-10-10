456d9a89ec340d0309b0421b6d81a70ec1c8aa89
Riley Lyman <rileylyman@berkeley.edu>
2018-10-09 20:15:00 -0700

    added show and remove support, plus cool banner

--- cmd/planner.go
+++ cmd/planner.go
@@ -13,6 +13,7 @@ const dataPath string = ".planner/tasks"
 
 var add string = "add"
 var remove string = "remove"
+var show string = "show"
 
 var expectedAddArgs = 1
 var expectedRemoveArgs = 1
@@ -22,20 +23,33 @@ var removeCommand = flag.NewFlagSet(remove, flag.ExitOnError)
 
 func main() {
 
+	if len(os.Args) == 1 {
+		fmt.Println("The Go Planner. For usage, type planner -h")
+		os.Exit(1)
+	}
+
 	switch prog := os.Args[1]; prog {
 	case add:
 		cmdAddTask()
 	case remove:
 		cmdRemoveTask()
+	case show:
+		cmdShowTasks()
+	case "-h":
+		fmt.Println("Valid programs: add, remove, and show. Use planner <program> -h for more info")
 	default:
 		fmt.Println("Invalid argument: ", prog)
 	}
 }
 
+func cmdShowTasks() {
+	listTasks()
+}
+
 func cmdAddTask() {
 
 	duedate := addCommand.String("d", "", "The due date for the task, which defaults to never.")
-	importance := addCommand.Int("i", 0, "The importance of the task, defaults to 0")
+	importance := addCommand.String("i", "0", "The importance of the task, defaults to 0")
 
 	parsedArgs := parseArgs(2, expectedAddArgs, addCommand)
 
@@ -44,18 +58,17 @@ func cmdAddTask() {
 	}
 
 	for _, arg := range parsedArgs {
-		if strings.Contains(arg, ";") || strings.Contains(arg, "`") {
+		if strings.Contains(arg, ";") || strings.Contains(arg, "~") {
 			fmt.Println("Invalid argument: [you cannot name a task or duedate that]")
 			os.Exit(1)
 		}
 	}
 	switch {
-	case strings.Contains(*duedate, ";") || strings.Contains(*duedate, "`"):
+	case strings.Contains(*duedate, ";") || strings.Contains(*duedate, "~"):
 		fmt.Println("Invalid argument: [you cannot name a task or duedate that]")
 	}
 
 	addTask(parsedArgs[0], *duedate, *importance)
-	fmt.Println(getRawTaskString())
 }
 
 func cmdRemoveTask() {
@@ -63,28 +76,126 @@ func cmdRemoveTask() {
 	parsedArgs := parseArgs(2, expectedRemoveArgs, removeCommand)
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Deleting task", parsedArgs[0], "are you sure you want to continue? (y/n) ")
+	fmt.Print("Deleting task [", parsedArgs[0], "]\n Are you sure you want to continue? (y/n) ")
 	text, _ := reader.ReadString('\n')
 
 	if text[0] == 'y' {
-		//removeTask(parsedArgs[0])
+		removeTask(parsedArgs[0])
 	} else  {
 		fmt.Println("Aborting.")
 	}
 
 }
 
-func addTask(taskName, taskDueDate string, taskImportance int) {
+func addTask(taskName, taskDueDate, taskImportance string) {
 	data := getRawTaskString()
-	data += taskName + ";" + taskDueDate + ";" + string(taskImportance)
+
+	taskData := ";" + taskName + "~" + taskDueDate + "~" + taskImportance
+
+	if strings.Contains(data, taskName) {
+		fmt.Print("Already added a task by this name. Would you like to update it? (y/n) ")
+		reader := bufio.NewReader(os.Stdin)
+		inp, _ := reader.ReadString('\n')
+
+		if inp[0] == 'y' {
+			fmt.Println("Overwriting previous task...")
+
+			taskIndex := strings.Index(data, taskName) - 1
+			left := data[:taskIndex]
+
+			var i int
+			for i = taskIndex + 1; i < len(data) && data[i] != ';'; i++ {}
+
+			right := data[i:]
+
+			data = left + taskData + right
+
+		} else {
+			fmt.Println("Aborting")
+			os.Exit(1)
+		}
+	} else {
+		data += taskData
+	}
 
 	toWrite := []byte(data)
 	err := ioutil.WriteFile(dataPath, toWrite, 0644)
 	check(err)
 }
 
+func removeTask(taskName string) {
+	data := getRawTaskString()
+
+	if strings.Contains(data, taskName) {
+		fmt.Println("Removing previous task...")
+
+		taskIndex := strings.Index(data, taskName) - 1
+		left := data[:taskIndex]
+
+		var i int
+		for i = taskIndex + 1; i < len(data) && data[i] != ';'; i++ {}
+
+		right := data[i:]
+
+		data = left + right
+	} else {
+		fmt.Println("There is no task by that name to remove.")
+		os.Exit(1)
+	}
+
+	toWrite := []byte(data)
+	err := ioutil.WriteFile(dataPath, toWrite, 0644)
+	check(err)
+
+}
+
 func listTasks() {
-	fmt.Println(getRawTaskString())
+
+	printBanner()
+
+	tasks := strings.Split(getRawTaskString(), ";")
+
+	var taskName, taskDueDate, taskImportance string
+
+	for index, task := range tasks {
+
+		if task == "" {continue;}
+		fields := strings.Split(task, "~")
+
+		if len(fields) == 0 {
+		continue
+		} else if len(fields) !=  3 {
+			fmt.Println("Something went wrong trying to print tasks. The data was \n", getRawTaskString())
+			os.Exit(1)
+		}
+
+		taskName = fields[0]
+		taskDueDate = fields[1]
+		taskImportance = fields[2]
+
+		fmt.Println("Task", index, ": |", taskName, "|")
+		fmt.Println("Due by:", taskDueDate)
+		fmt.Println("Importance is:", taskImportance, "\n")
+	}
+
+}
+
+func printBanner() {
+fmt.Println( "	 ,----,")
+fmt.Println( "      ,/   .`|")
+fmt.Println( "    ,`   .'  :                            ,-.")
+fmt.Println( "  ;    ;     /                        ,--/ /|")
+fmt.Println( ".'___,/    ,'                       ,--. :/ |")
+fmt.Println( "|    :     |              .--.--.   :  : ' /  .--.--.")
+fmt.Println( ";    |.';  ;  ,--.--.    /  /    '  |  '  /  /  /    '")
+fmt.Println( "`----'  |  | /       \\  |  :  /`./  '  |  : |  :  /`./")
+fmt.Println( "    '   :  ;.--.  .-. | |  :  ;_    |  |   \\|  :  ;_    ")
+fmt.Println( "    |   |  ' \\__\\/: . .  \\  \\    `. '  : |. \\  \\    `. ")
+fmt.Println( "    '   :  | , .--.; |   `----.    \\|  | ' \\ \\`----.   \\ ")
+fmt.Println( "    |   |.' /  /  ,.  |  /  /`--'  /'  : |--'/  /`--'  /")
+fmt.Println( "    '---'  ;  :   .'   \\'--'.     / ;  |,'  '--'.     /")
+fmt.Println( "           |  ,     .-./  `--'---'  '--'      `--'---'")
+fmt.Println( "            `--`---'                                \n\n\n")
 }
 
 func parseArgs(argsStart, expectedArgs int, flagSet *flag.FlagSet) (parsedArgs []string) {
